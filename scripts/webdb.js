c479647e23c7134126bfc54401ec89aef57755a3
Mike <mikekh@uw.edu>
2015-12-09 17:11:25 -0800

    added hackerIpsum removed blogArticles

--- scripts/webdb.js
+++ scripts/webdb.js
@@ -37,7 +37,7 @@ webDB.connect = function (database, title, size) {
 
 webDB.setupTables = function () {
   html5sql.process(
-    'TODO: Add SQL here',
+    'CREATE TABLE IF NOT EXISTS articles (id INTEGER PRIMARY KEY, title VARCHAR(255) NOT NULL, category VARCHAR(255) NOT NULL, author VARCHAR(255) NOT NULL, authorUrl VARCHAR(255), publishedOn DATETIME, body TEXT);',
     function() {
       // on success
       console.log('Success setting up tables.');
